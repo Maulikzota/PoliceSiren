7154f3dfbfe32ff27b392e407b047ae5974c1fe9
Maulik <maulikzota@hotmail.com>
2017-01-26 15:54:21 -0500

    Adding New AudioKit.

--- PoliceSiren/ViewController.swift
+++ PoliceSiren/ViewController.swift
@@ -22,7 +22,8 @@ class ViewController: UIViewController {
     var mic: AKMicrophone!
     var tracker: AKFrequencyTracker!
     var silence: AKBooster!
-    
+    var count=0
+    var avgFreq = 0.0
     
     let noteFrequencies = [16.35,17.32,18.35,19.45,20.6,21.83,23.12,24.5,25.96,27.5,29.14,30.87]
     let noteNamesWithSharps = ["C", "C♯","D","D♯","E","F","F♯","G","G♯","A","A♯","B"]
@@ -40,7 +41,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        AKAudioFile.cleanTempDirectory()
+//        AKAudioFile.cleanTempDirectory()
         
         
 //        AKSettings.audioInputEnabled = true
@@ -74,27 +75,33 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
+        
         if tracker.amplitude > 0.1 {
+            print("Amplitude: ",tracker.amplitude)
+            print("Frequency: ",tracker.frequency)
             frequencyLabel.text = String(format: "%0.1f Hz", tracker.frequency)
+            avgFreq+=tracker.frequency;
+            count+=1;
+            print("Count: ",count)
+            print("AvgFreq: ",avgFreq)
+//            var frequency = Float(tracker.frequency)
+//            while (frequency > Float(noteFrequencies[noteFrequencies.count-1])) {
+//                frequency = frequency / 2.0
+//            }
+//            while (frequency < Float(noteFrequencies[0])) {
+//                frequency = frequency * 2.0
+//            }
             
-            var frequency = Float(tracker.frequency)
-            while (frequency > Float(noteFrequencies[noteFrequencies.count-1])) {
-                frequency = frequency / 2.0
-            }
-            while (frequency < Float(noteFrequencies[0])) {
-                frequency = frequency * 2.0
-            }
-            
-            var minDistance: Float = 10000.0
-            var index = 0
-            
-            for i in 0..<noteFrequencies.count {
-                let distance = fabsf(Float(noteFrequencies[i]) - frequency)
-                if (distance < minDistance){
-                    index = i
-                    minDistance = distance
-                }
-            }
+//            var minDistance: Float = 10000.0
+//            var index = 0
+//            
+//            for i in 0..<noteFrequencies.count {
+//                let distance = fabsf(Float(noteFrequencies[i]) - frequency)
+//                if (distance < minDistance){
+//                    index = i
+//                    minDistance = distance
+//                }
+//            }
 //            let octave = Int(log2f(Float(tracker.frequency) / frequency))
 //            noteNameWithSharpsLabel.text = "\(noteNamesWithSharps[index])\(octave)"
 //            noteNameWithFlatsLabel.text = "\(noteNamesWithFlats[index])\(octave)"
